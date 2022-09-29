f22f7597dc3cae5758fc349eaf6c937907e740a1
Christopher Thompson <chris_thompson@bcit.ca>
2022-09-28 17:55:12 -0700

    Hello Demeter please bring back the harvest

---
+++ src/ca/bcit/comp2522/lab03/DemeterAirlinesDriver.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2522.lab03;
+
+/**
+ * This is the driver for your program.
+ *
+ * @author INSERT YOUR NAME HERE
+ * @version 202230
+ */
+public final class DemeterAirlinesDriver {
+
+    private DemeterAirlinesDriver() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+
+    }
+}
